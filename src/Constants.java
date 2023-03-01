df36d3812542d65b1be127f5f1f9f842b7fa8b37
Mourishitz <gabrielmorishita@hotmail.com>
2023-02-28 21:46:18 -0300

    🏗️ Adicionando constantes da chain

---
+++ src/Constants.java
@@ -0,0 +1,13 @@
+public class Constants {
+
+    private Constants() {
+
+    }
+    // The grater the difficulty, more 0's will appear leading the hash
+    public static final int DIFFICULTY = 4;
+    // Reward in the underlying cryptocurrency
+    public static final int REWARD = 10;
+    // Initial value for the first node previous hash (genesis node)
+    public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+
+}
